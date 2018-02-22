b86f53d8daf0d7f8b48c97079e06835456ca6a8b
TGS <stand-by-me@mail.com>
2018-02-22 20:46:48 +0800

    4 5

--- src/main/kotlin/7.kt
+++ src/main/kotlin/7.kt
@@ -9,17 +9,10 @@ fun main(args: Array<String>) {
     val observable: Observable<Any> = listOf("One", 2, "Three", "Four", 4.5, "Five", 6.0f).toObservable() //6
 
     observable.subscribe(observer)//7
-
-    val observableOnList: Observable<List<Any>> =
-            Observable.just(listOf("One", 2, "Three", "Four",
-                    4.5, "Five", 6.0f),
-                    listOf("List with Single Item"),
-                    listOf(1, 2, 3, 4, 5, 6))//8
-    observableOnList.subscribe(observer)//9
 }
 
 /*
-Subscribed to io.reactivex.internal.operators.observable.ObservableFromIterable$FromIterableDisposable@xxxxxxxx
+New Subscription
 Next One
 Next 2
 Next Three
@@ -28,9 +21,14 @@ Next 4.5
 Next Five
 Next 6.0
 All Completed
-Subscribed to io.reactivex.internal.operators.observable.ObservableFromArray$FromArrayDisposable@xxxxxxxx
-Next [One, 2, Three, Four, 4.5, Five, 6.0]
-Next [List with Single Item]
-Next [1, 2, 3, 4, 5, 6]
-All Completed
+ */
+
+/*
+
+    val observableOnList: Observable<List<Any>> =
+            Observable.just(listOf("One", 2, "Three", "Four",
+                    4.5, "Five", 6.0f),
+                    listOf("List with Single Item"),
+                    listOf(1, 2, 3, 4, 5, 6))//8
+    observableOnList.subscribe(observer)//9
  */
