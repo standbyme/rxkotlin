3399a8b410d0a7c4566ff6047074b2f3f53231dd
TGS <stand-by-me@mail.com>
2018-02-23 16:39:26 +0800

    rename

--- src/main/kotlin/3.2.kt
+++ src/main/kotlin/3.2.kt
@@ -1,6 +1,5 @@
-// https://www.safaribooksonline.com/library/view/reactive-programming-in/9781788473026/b9534ab3-f4c0-452e-a17e-fe5a178532f7.xhtml
-
 import io.reactivex.Observable
+
 
 fun main(args: Array<String>) {
     val observable: Observable<Int> = Observable.create<Int> {
@@ -8,7 +7,7 @@ fun main(args: Array<String>) {
         it.onNext(1)
         it.onNext(2)
         it.onNext(3)
-        it.onComplete()
+        it.onError(Exception("My Custom Exception"))
     }
 
     observable.subscribe(observer)
@@ -19,5 +18,5 @@ New Subscription
 Next 1
 Next 2
 Next 3
-All Completed
+Error Occured My Custom Exception
  */
