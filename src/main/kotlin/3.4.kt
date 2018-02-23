5e49041eea9171a625e7387fdd98e6308c233f77
TGS <stand-by-me@mail.com>
2018-02-23 17:02:43 +0800

    3.4

---
+++ src/main/kotlin/3.4.kt
@@ -0,0 +1,10 @@
+import io.reactivex.Observable
+
+fun main(args: Array<String>) {
+    val observable: Observable<Int> = Observable.create<Int> {
+        it.onComplete()
+        it.onNext(2)
+    }
+
+    observable.subscribe(observer)
+}
