64e2018d1b7ef079c07b599cbf6d57c572b8955d
TGS <stand-by-me@mail.com>
2018-02-23 17:54:14 +0800

    3.6 3.7

---
+++ src/main/kotlin/3.6.kt
@@ -0,0 +1,21 @@
+import io.reactivex.Observable
+
+fun main(args: Array<String>) {
+    Observable.just(54).subscribe(observer)
+    Observable.just(listOf(1, 2, 3)).subscribe(observer)
+    Observable.just(1, 2, 3).subscribe(observer)
+}
+
+/*
+New Subscription
+Next 54
+All Completed
+New Subscription
+Next [1, 2, 3]
+All Completed
+New Subscription
+Next 1
+Next 2
+Next 3
+All Completed
+ */
--- src/main/kotlin/3.7.kt
+++ src/main/kotlin/3.7.kt
@@ -1,20 +1,11 @@
 import io.reactivex.Observable
 
 fun main(args: Array<String>) {
-    Observable.just(54).subscribe(observer)
-    Observable.just(listOf(1, 2, 3)).subscribe(observer)
-    // Same Effect
     Observable.just(1, 2, 3).subscribe(observer)
     Observable.fromIterable(listOf(1, 2, 3)).subscribe(observer)
 }
 
 /*
-New Subscription
-Next 54
-All Completed
-New Subscription
-Next [1, 2, 3]
-All Completed
 New Subscription
 Next 1
 Next 2
