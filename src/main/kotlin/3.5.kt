7afa997ac4d6618f5eef5682ec5f6528dcff8dd1
TGS <stand-by-me@mail.com>
2018-02-23 17:44:47 +0800

    3.5

--- src/main/kotlin/3.5.kt
+++ src/main/kotlin/3.5.kt
@@ -3,8 +3,18 @@
 import io.reactivex.Observable
 
 fun main(args: Array<String>) {
-    val list = listOf("String 1", "String 2", "String 3", "String 4")
-    val observableFromIterable: Observable<String> = Observable.fromIterable(list)//1
+    val list = listOf(1, 2, 3, 4)
+    val observableFromIterable: Observable<Int> = Observable.fromIterable(list)//1
     // You can also use `list.toObservable()` instead of `Observable.fromIterable(list)`
     observableFromIterable.subscribe(observer)
 }
+
+
+/*
+New Subscription
+Next 1
+Next 2
+Next 3
+Next 4
+All Completed
+ */
