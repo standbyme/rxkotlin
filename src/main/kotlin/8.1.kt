b9c6084edb1414bbc56ed2eef827b81b42e6777c
TGS <stand-by-me@mail.com>
2018-03-04 16:39:43 +0800

    8

--- src/main/kotlin/8.1.kt
+++ src/main/kotlin/8.1.kt
@@ -1,45 +1,23 @@
-// 7.8.kt
-import io.reactivex.BackpressureStrategy
-import io.reactivex.Flowable
-import io.reactivex.schedulers.Schedulers
+import io.reactivex.Observable
+
+inline fun Any.toIntOrError(): Int = toString().toInt()
 
 fun main(args: Array<String>) {
-    val flowable: Flowable<Int> = Flowable.create<Int>({
-        for (i in 1..10) {
-            it.onNext(i) // it: FlowableEmitter
-        }
-        it.onComplete()
-    }, BackpressureStrategy.BUFFER)
-
-    flowable
-            .observeOn(Schedulers.newThread())
-            .subscribe(subscriber_3)
-
-    Thread.sleep(10000)
-
+    Observable.just(1, 2, "Errr", 3)
+            .map { it.toIntOrError() }
+            .subscribe(observer)
 }
 
 /*
-Create Flowable
-Flowable.create() takes two parameters to create an instance of Flowable. The following is the definition of the Flowable.create() method:
-
-    fun <T> create(source:FlowableOnSubscribe<T>,
-    mode:BackpressureStrategy):Flowable<T> {
-      //...
-    }
-First parameter is the source from where the emissions will generate,
-and the second one is BackpressureStrategy;
-it is an enum that helps supporting backpressure
-(it basically helps choosing which strategy to follow for backpressure)
-by caching/buffering or dropping some of the emissions if the downstream can't keep up.
-The enum BackpressureStrategy has five underlying options for different kinds of implementations of backpressure.
+New Subscription
+Next 1
+Next 2
+Error Occured For input string: "Errr"
  */
-
 /*
-BackpressureStrategy.MISSING: This leads to no backpressure implementation at all; downstream has to deal with backpressure overflows. This option is helpful while using the onBackpressureXXX() operator. We will learn this example later in this chapter.
-BackpressureStrategy.ERROR: This, again, leads to no backpressure implementation and signals MissingBackpressureException the very moment the downstream cannot keep up with the source.
-BackpressureStrategy.BUFFER: This buffers all the emissions in an unbounded buffer until the downstream is able to consume them. This can lead to OutOfMemoryError if there are a lot of emissions to buffer.
-BackpressureStrategy.DROP: This strategy will let you drop all the emissions while the downstream is busy and can't keep up; when the downstream finishes the previous operation, it'll get the very first emission after its finishing time, and will miss any emissions in between. For example, say the source is emitting five values, 1, 2, 3, 4, and 5 respectively, the downstream got busy after receiving 1 and while the source emitted 2, 3, and 4, it got ready just before the source emitted 5; the downstream will receive 5 only and will miss all remaining.
-BackpressureStrategy.LATEST: This strategy will let you drop all the emissions, but keeps the latest one while the downstream is busy and can't keep up; when the downstream finishes the previous operation it'll get the last emission just before it finished, and will miss any emissions in between. For example, say the source is emitting five values 1, 2, 3, 4, and 5 respectively, the downstream got busy after receiving 1 and while the source emitted 2, 3, and 4, it got ready just before the source emitted 5; the downstream will receive both of them
-(if it didn't again get busy after receiving 4, that it can't receive 5).
+However, the problem with the onError event is that the error is emitted to the downstream consumer chain
+, and the subscription is terminated instantly.
+The program throws an exception in the map operator when the string Errr is emitted from the Observable.
+The exception was caught by the onError handler
+, but the Subscription doesn't get any further emissions.
  */
